433c48784030bbcd2a86a4983fd0a9c6fe78926d
FarahSamreen <84279491+FarahSamreen@users.noreply.github.com>
2022-11-04 21:25:19 +0530

    Update stafflogin.php

--- stafflogin.php
+++ stafflogin.php
@@ -1,106 +1,52 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+<html lang="en">
     <style>
-      body {
-        font-family: Arial, Helvetica, sans-serif;
-      }
-      form {
-        border: 3px solid #f1f1f1;
-      }
-
-      input[type="text"],
-      input[type="password"] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-      }
-
-      button {
-        background-color: #04aa6d;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-      }
-
-      button:hover {
-        opacity: 0.8;
-      }
-
-      .cancelbtn {
-        width: auto;
-        padding: 10px 18px;
-        background-color: #f44336;
-      }
-
-      .imgcontainer {
-        text-align: center;
-        margin: 24px 0 12px 0;
-      }
-
-      img.avatar {
-        width: 40%;
-      }
-
-      .container {
-        padding: 16px;
-      }
-
-      span.psw {
-        float: right;
-        padding-top: 16px;
-      }
-
-      /* Change styles for span and cancel button on extra small screens */
-      @media screen and (max-width: 300px) {
-        span.psw {
-          display: block;
-          float: none;
-        }
-        .cancelbtn {
-          width: 100%;
-        }
-      }
+    .logo {
+      width: 300px;
+      height: auto;
+    }
     </style>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <a href="index.php"><img src="Images/logo.png" alt="bmshospital" class="logo" /></a>
+    <hr
+      style="
+        height: 2px;
+        border-width: 0;
+        color: rgb(158, 147, 147);
+        background-color: rgb(158, 147, 147);
+      "
+    />
+    <title>Login</title>
   </head>
   <body>
-    <h2>Login Form</h2>
+    
+    <div class="container my-4">
+     <h1 class="text-center">Login</h1>
+     <form action="backlogin.php" method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" maxlength="11" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+            
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" maxlength="23" class="form-control" id="password" name="password">
+        </div>
+         
+        <button type="submit" class="btn btn-primary">Login</button>
+     </form>
+    </div>
 
-    <form action="login.php" method="post">
-      <div class="imgcontainer">
-   <a href="index.php"><img src="Images/logo.png" alt="bmshospital" class="logo" /></a>
-      </div>
-
-      <div class="container">
-        <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required />
-
-        <label for="psw"><b>Password</b></label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-        />
-
-        <button type="submit">Login</button>
-      </div>
-
-      <div class="container" style="background-color: #f1f1f1">
-        <button type="button" class="cancelbtn">Cancel</button>
-        <span class="psw">Forgot <a href="#">password?</a></span>
-      </div>
-    </form>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
